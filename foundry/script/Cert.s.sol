ca5ab71322af5bc54ebe563bd5bb8d3343b49240
tr1sm0s1n <mobinm.mohanan@gmail.com>
2025-11-12 15:15:48 +0530

    🔨 | use console.log() in scripts if required
    ---------
    Signed-off-by: tr1sm0s1n <mobinm.mohanan@gmail.com>

--- foundry/script/Cert.s.sol
+++ foundry/script/Cert.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {Script, console} from "forge-std/Script.sol";
+import {Script} from "forge-std/Script.sol";
 
 contract CertScript is Script {
     function setUp() public {}
--- foundry/test/Cert.t.sol
+++ foundry/test/Cert.t.sol
@@ -12,24 +12,28 @@ contract CounterTest is Test {
     }
 
     function test_Issue() public {
+        console.log("Issuing certificate for ID: 105");
         vm.expectEmit(true, false, false, false);
         emit Cert.Issued("MBCC", 105, "2025");
         cert.issue(105, "Jasmine", "MBCC", "S", "2023");
     }
 
     function test_Certificates() public {
+        console.log("Issuing certificate for ID: 395");
         cert.issue(395, "Levy", "MBCC", "A", "2023");
         string memory name;
         string memory course;
         string memory grade;
         string memory date;
-        (name, course, grade, date) = cert.Certificates(395);
+        console.log("Fetching certificate for ID: 395");
+        (name, course, grade, date) = cert.certificates(395);
         Cert.Certificate memory cc = Cert.Certificate(
             "Levy",
             "MBCC",
             "A",
             "2023"
         );
+
         assertEq(name, cc.name);
         assertEq(course, cc.course);
         assertEq(grade, cc.grade);
@@ -39,6 +43,7 @@ contract CounterTest is Test {
     function test_Revert() public {
         vm.expectRevert("Access Denied");
         vm.prank(address(0));
+        console.log("Issuing certificate for ID: 250");
         cert.issue(250, "Pepper", "MBCC", "B", "2022");
     }
 }
